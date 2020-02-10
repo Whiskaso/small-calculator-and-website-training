a987a1b43ab0203ffab61c4b8fa58c5c309a1c88
Whiskaso <marcinlakomy9@gmail.com>
2020-02-10 20:03:01 +0100

    add currency select

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
-function getInputsValues() {
+function calculatingPrice() {
 
-    var coursePriceInDollars = document.getElementById('priceInput').value;     // variable with price course from first input 
+    var coursePrice = document.getElementById('priceInput').value;     // variable with price course from input 
 
-    var currencyToPln = document.getElementById('currencyInput').value;         //variable with currency calculated to PLN from second input
+    var currency = document.getElementById('currency').value;       // variable with currency from select 
 
-    var coursePriceInZl = coursePriceInDollars * currencyToPln;         
+    if (currency == 'PLN') {
+        currency = 1;
+    } else if (currency == 'USD') {
+        currency = 3.19;
+    } else if (currency == 'GBP') {
+        currency = 5.05;
+    } else if (currency == 'CHF') {
+        currency = 4;
+    } else if (currency == 'CZK') {
+        currency = 0.17;
+    }
+
+    var coursePriceInPln = coursePrice * currency;  
     
-    if (coursePriceInZl > 300) {
-        document.getElementById('placeForPrice').innerHTML = 'DONT BUY IT ! The price is ' + coursePriceInZl + 'zł !';
+    if (coursePriceInPln > 300) {
+        document.getElementById('placeForPrice').innerHTML = 'DONT BUY IT ! The price is ' + Math.round(coursePriceInPln) + 'PLN !';
     } else {
-        document.getElementById('placeForPrice').innerHTML = 'You can buy it ;) The price is ' + coursePriceInZl + 'zł !';
+        document.getElementById('placeForPrice').innerHTML = 'You can buy it ;) The price is ' + Math.round(coursePriceInPln) + 'PLN !';
     }
 }
 
