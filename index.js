a0705e85a6bcb003eac7d74b59660fdabe5d0c7d
Whiskaso <marcinlakomy9@gmail.com>
2020-02-12 21:51:49 +0100

    add generating table from XML file using AJAX - DOESNT WORK

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//function calculating course price
+
 function calculatingPrice() {
 
     var coursePrice = document.getElementById('priceInput').value;     // variable with price course from input 
@@ -25,3 +27,36 @@ function calculatingPrice() {
     }
 }
 
+// function for comunication with server, getting XML document - AJAX 
+
+function printCoursesList() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            generateTable(this);
+        }
+    };
+    xhttp.open('GET', 'courses.xml', true);
+    xhttp.send();
+}
+
+// generating table with courses from XML doc 
+
+function generateTable(xml) {
+    var i;
+    var xmlDoc = xml.responseXML;
+    var table = '<tr><th>Title</th><th>Author</th><th>Learning Area</th><th>Price</th></tr>';
+    var x = xmlDoc.getElementsByTagName('course');
+    for (i = 0; i < x.lenght; i++) {
+        table += '<tr><td>' + 
+        x[i].getElementsByTagName('title')[0].childNodes[0].nodeValue + 
+        '</td></tr>' +
+        x[i].getElementsByTagName('author')[0].childNodes[0].nodeValue + 
+        '</tr></td>' +
+        x[i].getElementsByTagName('learningArea')[0].childNodes[0].nodeValue + 
+        '</tr></td>' +
+        x[i].getElementsByTagName('price')[0].childNodes[0].nodeValue + 
+        '</tr></td>';
+    }
+    document.getElementById('printTable').innerHTML = table; 
+}
